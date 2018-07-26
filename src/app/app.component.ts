7f3f5dc4ad863bd2079255f941baa137317cea74
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2018-07-26 09:32:48 -0400

    Creating service for posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostsService } from './services/posts.service';
+import { Subscription } from 'rxjs';
+import { Post } from './models/Post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
-  posts: any[];
+  posts: Post[];
+  postsSubscription: Subscription;
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit() {
-    this.posts = this.postsService.posts;
+    this.postsSubscription = this.postsService.postsSubject.subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+    this.postsService.emitPostsSubject();
+  }
+
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
   }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,8 +1,11 @@
 import { Post } from '../models/Post.model';
+import { Subject } from 'rxjs';
 
 export class PostsService {
 
-  posts = [
+  postsSubject = new Subject<Post[]>();
+
+  private posts = [
     // tslint:disable-next-line:max-line-length
     new Post('Quare hoc quidem praeceptum', 'Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.', 4, Date.now()),
     // tslint:disable-next-line:max-line-length
@@ -11,13 +14,19 @@ export class PostsService {
     new Post('Abusus enim multitudine', 'Abusus enim multitudine hominum, quam tranquillis in rebus diutius rexit, ex agrestibus habitaculis urbes construxit multis opibus firmas et viribus, quarum ad praesens pleraeque licet Graecis nominibus appellentur, quae isdem ad arbitrium inposita sunt conditoris, primigenia tamen nomina non amittunt, quae eis Assyria lingua institutores veteres indiderunt.', 8, Date.now())
   ];
 
+  emitPostsSubject() {
+    this.postsSubject.next(this.posts.slice());
+  }
+
   likePost(i: number) {
     this.posts[i].loveIts++;
+    this.emitPostsSubject();
     console.log(this.posts[i].loveIts);
   }
 
   unlikePost(i: number) {
     this.posts[i].loveIts--;
+    this.emitPostsSubject();
     console.log(this.posts[i].loveIts);
   }
 
